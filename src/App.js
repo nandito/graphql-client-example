d201ccd7db0b22f8c69d45973ca1c4e62a178bb3
Nandor Biro <dev@nandito.info>
2018-10-15 21:08:37 +0200

    Add Ant.design

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
+import { Layout } from 'antd'
+import './App.css'
 
 class App extends Component {
   render() {
+    const { Header, Content, Footer } = Layout
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>
-            Client Example
-          </h1>
-        </header>
-      </div>
+      <Layout>
+        <Header>Client Example</Header>
+        <Layout>
+          <Content style={{ padding: '0 50px' }}>
+            <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+              Content
+            </div>
+          </Content>
+        </Layout>
+        <Footer style={{ textAlign: 'center' }}>
+          Client Example by <a href="https://nandito.info">nandito</a>
+        </Footer>
+      </Layout>
     )
   }
 }
