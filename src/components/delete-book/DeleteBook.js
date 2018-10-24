533a40bcb13051ab3db8b5a3eaa4918659b751b9
Nandor Biro <dev@nandito.info>
2018-10-24 21:34:17 +0200

    Update cache when a book is removed

--- src/components/delete-book/DeleteBook.js
+++ src/components/delete-book/DeleteBook.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Mutation } from 'react-apollo'
 import { Button } from 'antd'
-import { DELETE_BOOK } from '../../graphql'
+import { DELETE_BOOK, GET_BOOKS } from '../../graphql'
 
 export default ({ id }) => (
-  <Mutation mutation={DELETE_BOOK}>
+  <Mutation
+    mutation={DELETE_BOOK}
+    update={(cache, { data }) => {
+      const remainingBooks = data.deleteBook
+
+      cache.writeQuery({
+        query: GET_BOOKS,
+        data: { books: remainingBooks }
+      })
+    }}
+  >
     {(deleteBook, { loading }) => (
       <Button
         disabled={loading}
